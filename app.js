96b54c8bba85d124e26ef9df0e8c196c85a4e05c
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-11 09:53:08 +0200

    encapsulation in order to avoid contradiction from this code with another (having the same name e.g. person)

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 var person = "Ahmed";
-logPerson(person);
+// to log person
+ 
+shoaibApp.logPerson(); // Shoaib Ali (avoided to log person Ahmed). this concept is a globalnamespace
+
+shoaibApp.person = person;
+shoaibApp.logPerson(); // Ahmed
+
+
+
+
+
--- utility.js
+++ utility.js
@@ -1,4 +1,7 @@
-var person = "Shoaib Ali";
-function logPerson(){
-    console.log(person);  
+// It is a utility or Someone else code, that i got it from the internet
+// Encapsulation (to avoid contradiction from this code with another having the same name person)
+var shoaibApp = {};
+shoaibApp.person = "Shoaib Ali";
+shoaibApp.logPerson = function(){
+    console.log(shoaibApp.person);  
 }
