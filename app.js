98023678caa010586ce3e35685efa9ddd2b1a07f
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-11 20:41:27 +0200

    interpolation

--- app.js
+++ app.js
@@ -1,43 +1,5 @@
-// shoaibApp is an angular module
-var shoaibApp = angular.module("myApp", ['ngResource', 'ngMessages']);
-/*
-shoaibApp.person = "Shoaib Ali";
-shoaibApp.logPerson = function(){
-    console.log(shoaibApp.person);
-    
-}
-shoaibApp.logPerson();
+var shoaibApp = angular.module("myApp", []);
+shoaibApp.controller('mainController',['$scope', function($scope){
+  $scope.name="Shoaib";
+}]);
 
-console.log("-----------Angular Module Object-------");
-console.log(shoaibApp);
-*/
-/*
-shoaibApp.greeting = function(en, dk){
-    this.englishGreeting = en;
-    this.danishGreeting = dk;
-}
-
-var myGreeting = new shoaibApp.greeting('Hello, World!', 'Hej, Verden!');
-// console.log("myGreeting: ",myGreeting);
-
-shoaibApp.controller('mainController', function($scope, myGreeting){
-    $scope.greeting = myGreeting.englishGreeting;
-});
-shoaibApp.controller('secondaryController',function($scope){
-    $scope.greeting = myGreeting.danishGreeting;
-});
-*/
-
-shoaibApp.controller("mainController", function ($scope, $log, $filter, $resource) {
-  // model or data
-  $scope.greeting = "hej verden";
-  $log.warn($scope.greeting);
-    console.log($resource);
-    
-  $scope.greeting = $filter("uppercase")($scope.greeting);
-});
-
-shoaibApp.controller("secondaryController", function ($scope, $filter) {
-  $scope.greeting = "hejsa";
-  $scope.greeting = $filter("uppercase")($scope.greeting);
-});
