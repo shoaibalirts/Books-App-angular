24aa8e83ddb8d442d5010c3b9585b63f6c1b2872
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-11 21:26:59 +0200

    view change the model and model change the view. It is a two-way binding

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var shoaibApp = angular.module("myApp", []);
+/*
 shoaibApp.controller('mainController',['$scope','$timeout', function($scope, $timeout){
   $scope.name="Shoaib";
   $timeout(function(){
@@ -7,4 +8,12 @@ shoaibApp.controller('mainController',['$scope','$timeout', function($scope, $ti
       
   }, 3000);
 }]);
+*/
+shoaibApp.controller('mainController',['$scope','$filter', function($scope,$filter){
+  $scope.twitterRoute='';
+  $scope.lowercaseHandle = function(){
+    return $filter("lowercase")($scope.twitterRoute);
+  }
 
+  
+}]);
