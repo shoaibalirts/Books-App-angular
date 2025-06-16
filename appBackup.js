a196fe5f87d6a6f537eeb46f88a69bfa3830f560
shoaibalirts <shoaibali.rts@gmail.com>
2025-06-16 12:31:14 +0200

    created custom directive named myBookDetail

--- appBackup.js
+++ appBackup.js
@@ -1,6 +1,7 @@
-var spaApp = angular.module("myApp", ["ngRoute"]);
+var spaApp = angular.module("myApp", ['ngRoute']);
 
 spaApp.config(function ($routeProvider) {
+  
   $routeProvider
     .when("/", {
       templateUrl: "pages/book.html",
@@ -19,85 +20,60 @@ spaApp.config(function ($routeProvider) {
       controller: "bookDetailController",
     })
     .when("/book/:bookId/ch/:num", {
-      templateUrl: "pages/chapter-detail.html",
+      templateUrl: "pages/chapterdetail.html",
       controller: "chapterDetailController",
     });
   // console.log($routeProvider);
+
 });
 
 // creating a service
-spaApp.service("bookNameService", function () {
+spaApp.service('bookNameService', function(){
   var self = this;
   this.bookName = "Learn Angular under the hood";
-  this.bookNameLength = function () {
+  this.bookNameLength = function(){
     return self.bookName.length;
-  };
+  }
 });
 
 spaApp.controller("bookController", [
-  "$scope",
-  "bookNameService",
+  "$scope","bookNameService",
   function ($scope, bookNameService) {
     $scope.bookName = bookNameService.bookName;
-    $scope.$watch("bookName", function () {
+    $scope.$watch('bookName', function(){
       bookNameService.bookName = $scope.bookName;
     });
   },
 ]);
 spaApp.controller("chapterController", [
-  "$scope",
-  "bookNameService",
+  "$scope","bookNameService",
   function ($scope, bookNameService) {
     $scope.bookName = bookNameService.bookName;
-    $scope.$watch("bookName", function () {
+    $scope.$watch('bookName', function(){
       bookNameService.bookName = $scope.bookName;
     });
   },
 ]);
 
 spaApp.controller("bookDetailController", [
-  "$scope",
-  "$routeParams",
+  "$scope","$routeParams",
   function ($scope, $routeParams) {
-    $scope.bookDescription =
-      "This book is for those who do not imitate, rather understand angular, one of the most popular JavaScript frameworks.";
-    $scope.bookId = $routeParams.bookId;
+    $scope.bookDescription = "This book is for those who do not imitate, rather understand angular, one of the most popular JavaScript frameworks.";
+    $scope.bookId=$routeParams.bookId;
     // $scope.chNum = $routeParams.num;
     // console.log("Book Id: ",$routeParams.bookId);
     // console.log("Chapter Number of this book: ",$routeParams.num);
+
   },
 ]);
 spaApp.controller("chapterDetailController", [
-  "$scope",
-  "$routeParams",
+  "$scope","$routeParams",
   function ($scope, $routeParams) {
     $scope.bookId = $routeParams.bookId;
     $scope.chNum = $routeParams.num;
-
+    
     // console.log("Book Id: ",$routeParams.bookId);
     // console.log("Chapter Number of this book: ",$routeParams.num);
-  },
-]);
 
-var detail = [
-  { name: "Learn and Understand JavaScript", chapters: 10 },
-  { name: "Learn and Understand React", chapters: 15 },
-  { name: "Learn and Understand Vue", chapters: 18 },
-  { name: "Learn and Understand Angular", chapters: 20 },
-];
-const bookNum = 1;
-spaApp.controller("myBookDetailController", [
-  "$scope",
-  function ($scope) {
-    $scope.bookName = detail[bookNum].name;
-    $scope.totalChapters = detail[bookNum].chapters;
   },
 ]);
-spaApp.directive("myBookDetail", function () {
-  return {
-    restrict: "A",
-    templateUrl: function (elem, attr) {
-      return "pages/book-" + attr.type + ".html";
-    },
-  };
-});
